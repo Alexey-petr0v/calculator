9e0a17382c239f75f0dd7bbe70b6355ad6577486
Alexey-petr0v <59523324+Alexey-petr0v@users.noreply.github.com>
2020-09-18 12:25:09 +0700

    Доделан процедурный подход, начат ООП-подход

---
+++ objects.js
@@ -0,0 +1,48 @@
+
+
+        Number.prototype.toFixed = function(x){ return Math.round(this * Math.pow(10, x)) / Math.pow(10, x);}
+// result (=)
+class Result {
+    constructor() {
+        this.input = document.getElementById("input_obj");
+        this.result = document.getElementById("result_obj");
+        
+    }
+    calculate_obj() {
+        this.result.value = (eval(this.input.value)).toFixed(20)
+    }
+}
+
+
+
+
+
+if (document.getElementById("buttons_table_obj")){
+
+    // result (=)
+    let result_obj = new Result();
+    document.getElementById("button_obj").addEventListener("click", result_obj.calculate_obj(), false);
+
+
+    // clear_all [С]
+    document.getElementById("clear_all_obj").addEventListener("click", clear_all_obj, false);
+    function clear_all_obj() {
+        result.value = '';
+        input.value = '';
+    }
+    
+    // clear [←]
+    document.getElementById("clear_obj").addEventListener("click", clear_obj, false);
+    function clear_obj() {
+        input.value = input.value.slice(0, -1);
+    }
+
+    // add [numbers, +, -, /, *, ., ), (]
+    var numbers = document.querySelectorAll(".add_obj");
+    for (var i = 0; i < numbers.length; i++) {
+        numbers[i].addEventListener("click", function () {
+            input.value = input.value + this.innerHTML;
+        }, false);
+    }
+}
+ 
---
+++ procedure.js
@@ -0,0 +1,35 @@
+
+
+if (document.getElementById("buttons_table")) {
+    Number.prototype.toFixed = function(x){ return Math.round(this * Math.pow(10, x)) / Math.pow(10, x);}
+    let input = document.getElementById("input");
+    let result = document.getElementById("result");
+
+    // result (=)
+    document.getElementById("button").addEventListener("click", calculate, false);
+    function calculate() {
+        result.value = (eval(input.value)).toFixed(20)
+    }
+
+    // clear_all [С]
+    document.getElementById("clear_all").addEventListener("click", clear_all, false);
+    function clear_all() {
+        result.value = '';
+        input.value = '';
+    }
+    
+    // clear [←]
+    document.getElementById("clear").addEventListener("click", clear, false);
+    function clear() {
+        input.value = input.value.slice(0, -1);
+    }
+
+    // add [numbers, +, -, /, *, ., ), (]
+    var numbers = document.querySelectorAll(".add");
+    for (var i = 0; i < numbers.length; i++) {
+        numbers[i].addEventListener("click", function () {
+            input.value = input.value + this.innerHTML;
+        }, false);
+    }
+}
+ 
