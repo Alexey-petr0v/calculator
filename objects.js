257f917e4cc0638507af484f1cc4dbcbbc30e32c
git-user-111 <user1002233@gmail.com>
2021-09-14 16:30:49 +0700

    Доделан ООП-подход

--- objects.js
+++ objects.js
@@ -1,48 +1,93 @@
 
 
-        Number.prototype.toFixed = function(x){ return Math.round(this * Math.pow(10, x)) / Math.pow(10, x);}
-// result (=)
-class Result {
+Number.prototype.toFixed = function(x){ return Math.round(this * Math.pow(10, x)) / Math.pow(10, x);}
+
+class ClickListener {
+    constructor(calculator) {
+        this.calculator = calculator;
+    }
+    // result (=)
+    addResultListener(id) {
+        document.getElementById(id).addEventListener("click", this.calculator.calculate, false);
+    }
+    // clear_all [С]
+    addClearAllListener(id) {
+        document.getElementById(id).addEventListener("click", this.calculator.clearAllObj, false);
+    }
+    // clear [←]
+    addClearListener(id) {
+        document.getElementById(id).addEventListener("click", this.calculator.clearObj, false);
+    }
+    // add [numbers, +, -, /, *, ., ), (]
+    addOtherSymbolsListener(symbolClass) {
+        var numbers = document.querySelectorAll(symbolClass);
+        for (var i = 0; i < numbers.length; i++) {
+            numbers[i].addEventListener("click", this.calculator.addOtherSymbol, false);
+        }
+    }
+    addListenes() {
+        this.addResultListener("button_obj");
+        this.addClearAllListener("clear_all_obj");
+        this.addClearListener("clear_obj");
+        this.addOtherSymbolsListener(".add_obj");
+    }
+}
+
+class Calculator {
+    constructor() {
+        this.resultValue = "";
+        this.inputValue = "";
+    }
+    // result (=)
+    calculate() {
+        this.viewer = new Viewer();
+        this.resultValue = (eval(this.viewer.input.value)) ? (eval(this.viewer.input.value)).toFixed(10) : 0;
+        this.viewer.viewResult(this.resultValue, null)
+        this.viewer = null;
+    }
+
+    // clear_all [С]
+    clearAllObj() {
+        this.viewer = new Viewer();
+        this.resultValue = '';
+        this.inputValue = '';
+        this.viewer.viewResult(this.resultValue, this.inputValue)
+        this.viewer = null;
+    }
+    
+    // clear [←]
+    clearObj() {
+        this.viewer = new Viewer();
+        this.inputValue = this.viewer.input.value.slice(0, -1);
+        this.viewer.viewResult(null, this.inputValue)
+        this.viewer = null;
+    }
+
+    addOtherSymbol(event) {
+        this.viewer = new Viewer();
+        this.inputValue = this.viewer.input.value + event.target.innerHTML;
+        this.viewer.viewResult(null, this.inputValue)
+        this.viewer = null;
+    }
+}
+
+class Viewer {
     constructor() {
         this.input = document.getElementById("input_obj");
         this.result = document.getElementById("result_obj");
-        
     }
-    calculate_obj() {
-        this.result.value = (eval(this.input.value)).toFixed(10)
+    viewResult(resultValue, inputValue) {
+        this.input = document.getElementById("input_obj");
+        this.result = document.getElementById("result_obj");
+        this.result.value = resultValue ? resultValue : "";
+        this.input.value = inputValue ? inputValue : "";
     }
 }
 
 
-
-
-
 if (document.getElementById("buttons_table_obj")){
-
-    // result (=)
-    let result_obj = new Result();
-    document.getElementById("button_obj").addEventListener("click", result_obj.calculate_obj(), false);
-
-
-    // clear_all [С]
-    document.getElementById("clear_all_obj").addEventListener("click", clear_all_obj, false);
-    function clear_all_obj() {
-        result.value = '';
-        input.value = '';
-    }
-    
-    // clear [←]
-    document.getElementById("clear_obj").addEventListener("click", clear_obj, false);
-    function clear_obj() {
-        input.value = input.value.slice(0, -1);
-    }
-
-    // add [numbers, +, -, /, *, ., ), (]
-    var numbers = document.querySelectorAll(".add_obj");
-    for (var i = 0; i < numbers.length; i++) {
-        numbers[i].addEventListener("click", function () {
-            input.value = input.value + this.innerHTML;
-        }, false);
-    }
+    const calculator = new Calculator();
+    const clickListener = new ClickListener(calculator);
+    clickListener.addListenes();
 }
  
